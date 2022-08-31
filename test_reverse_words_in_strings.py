7488fccfeee25a488c9e27e9501879a1710a5571
twcrone <twcrone@gmail.com>
2022-08-30 20:23:24 -0400

    Add solution to Reverse Words In Strings

--- test_reverse_words_in_strings.py
+++ test_reverse_words_in_strings.py
@@ -1,8 +1,33 @@
 def reverse_words_in_strings(s):
-    if s == '' or s == None:
+    if s == '' or s is None:
         return s
-    return s
+    words = ''
+    space = ' '
+    i = 0
+    current = s[i]
+    i += 1
+    is_ws = current.isspace()
+
+    while i < len(s):
+        c = s[i]
+        i += 1
+        if (is_ws and c == space) or (not is_ws and c != space):
+            current += c
+        else:
+            words = current + words
+            current = c
+            is_ws = current.isspace()
+    words = current + words
+    return words
 
 
 def test_nothing():
     assert reverse_words_in_strings('') == ''
+
+
+def test_single_char():
+    assert reverse_words_in_strings('c') == 'c'
+
+
+def test_basic_sentence():
+    assert reverse_words_in_strings("What is your name?") == "name? your is What"
